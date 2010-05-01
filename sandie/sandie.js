d2187202578bc21651c682544749d30f3bc24ab4
Premasagar <p@dharmafly.com>
2010-05-01 06:53:17 -0400

    getScripts single & multiple further integrated

--- sandie/sandie.js
+++ sandie/sandie.js
@@ -35,27 +35,22 @@ this.sandie = (function(){
     /*
     *   getScript
     */
-    function getScript(src, callback, arg3, arg4){    
+    function getScript(src, callback, targetWindow, inOrder){
+        targetWindow = targetWindow || window;
+        callback = callback || function(){};
+    
         /**
          * Load a script into a <script> element
          * @param {String} src The source url for the script to load
          * @param {Function} callback Called when the script has loaded
-         * TODO: 
-         * 1) Look in DOM for script element with that src already, and don't load it 
-         *    again if found (allows multiple Sqwidget scripts not to keep loading jQuery, etc)
-         * 2) {url: src, callback: fn} objects to allow specific callbacks for particular scripts; 
-         * 3) {lookForScriptSrcInDOM:false} options object; 
-         * 4) callback function when all scripts loaded
          */
-        function getSingleScript(src, callback, targetWindow){
+        function single(src, callback){
             var
-                self = this,
-                document = (targetWindow || window).document,
+                document = targetWindow.document,
                 head = document.getElementsByTagName('head')[0],
                 script = document.createElement('script'),
                 loaded;
-            
-            callback = callback || function(){};
+                
             script.src = src;
             script.onload = script.onreadystatechange = function(){
                 var state = this.readyState;
@@ -65,7 +60,7 @@ this.sandie = (function(){
                     // head.removeChild(script); // Worth removing script element once loaded?
                     
                     loaded = true;
-                    callback.call(self);
+                    callback.call(targetWindow);
                 }
             };
             head.appendChild(script);
@@ -83,34 +78,24 @@ this.sandie = (function(){
          * @param {Boolean} inOrder - if true, load scripts in given order
          */
 
-        function getMultipleScripts(srcs, callback, inOrder, targetWindow){
+        function multiple(srcs, callback, inOrder, targetWindow){
 			var
-                self = this,
-                args = arguments,
 			    length = srcs.length,
 				loaded = 0,
 				checkIfComplete;
 
 	        callback = callback || function(){};
-	        if (!targetWindow){
-	            if (typeof args[args.length-1] === 'object'){
-	                targetWindow = args[args.length-1];
-	            }
-	            else {
-                    targetWindow = window;
-	            }
-	        }
 			
 			if (inOrder === true) {
 				// Recursive, each callback re-calls getScripts
 				// with a shifted array.
-				getSingleScript(srcs.shift(), function(){
+				single(srcs.shift(), function(){
 					if (length === 1){
-						callback.call(self);
+						callback.call(targetWindow);
 					}
 					else {
 					    // preserve inOrder when recursing
-						getMultipleScripts(srcs, callback, true);
+						multiple(srcs, callback, true);
 					}
 				}, targetWindow);
 			}
@@ -118,22 +103,20 @@ this.sandie = (function(){
 				// Plain old loop
                 checkIfComplete = function(){
 					if (++loaded === length){
-						callback.call(self);
+						callback.call(targetWindow);
 					}
 				};
 				
 				// Doesn't call callback until all scripts have loaded.
 				for (var i = 0; i < length; ++i){
-					getSingleScript(srcs[i], checkIfComplete, targetWindow);
+					single(srcs[i], checkIfComplete, targetWindow);
 				}
 			}			
 		}
 
 		// **
 
-		var method = typeof src === 'string' ?
-                getSingleScript :
-                getMultipleScripts;
+		var method = (typeof src === 'string') ? single : multiple;
         return method.apply(this, arguments);
     }
     /*
