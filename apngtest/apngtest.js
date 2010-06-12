70fb80c6bfa3328d3b892f26d79ef789a072c235
Premasagar Rose <p@dharmafly.com>
2010-06-12 16:38:14 -0400

    apngtest: additional check for dataURI support; demo page added

--- apngtest/apngtest.js
+++ apngtest/apngtest.js
@@ -11,7 +11,8 @@
     detects browser support for Animated PNG images (APNG)
 
     notes
-        requires a callback that will be passed either true, false or null (if browser doesn't support Canvas, which is required for the test)
+        requires a callback that will be passed either true, false or null (the value will be null if the browser doesn't support the requirements for the test - i.e. dataURIs and Canvas)
+        
         APNG is supported in Firefox 3 and Opera 9.5 (as of 2010-06-12)
 
 */
@@ -26,12 +27,17 @@ function apngtest(callback){
         canvasContext = canvas.getContext('2d');
         img = document.createElement('img');
         img.src = apngSrc;
-        img.onload = function(){
-            canvasContext.drawImage(img, 0, 0);
-            callback(canvasContext.getImageData(0, 0, 1, 1).data[3] === 0); // true if apng is supported
+        img.onload = img.onerror = function(){
+            if (img.width === 1 && img.height === 1){
+                canvasContext.drawImage(img, 0, 0);
+                callback(canvasContext.getImageData(0, 0, 1, 1).data[3] === 0); // true if APNG is supported
+            }
+            else { // browser doesn't support dataURIs
+                callback(null);
+            }
         };
     }
-    else { // canvas not supported, so impossible to conduct test
+    else { // browser doesn't support canvas
         callback(null);
     }
 }
