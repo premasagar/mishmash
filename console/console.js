7a5c656673885622fefcecadbf5f4e186870ec17
Premasagar Rose <p@dharmafly.com>
2011-03-04 00:40:25 -0500

    console: support for Air Introspector, old Opera code removed; new WebKit update

--- console/console.js
+++ console/console.js
@@ -30,81 +30,52 @@ var _ = (function(){
     var window = self,
         ua = window.navigator.userAgent,
         console = window.console,
-        opera = window.opera,
-        debug, log;
+        air = window.air && window.air.Introspector,
+        debug = console && console.debug,
+        log = console && console.log,
+        method,
+        msg = "Debugging on";
     
-    // Doesn't support console API
-    if (!console){
-        // Opera 
-        return (opera && opera.postError) ?
-             function(){
-                 var i, argLen, log = opera.postError, args = arguments, arg, subArgs, prop;
-                 log(args);
-                 
-                 argLen = args.length;
-                 for (i=0; i < argLen; i++){
-                     arg = args[i];
-                     if (typeof arg === "object" && arg !== null){
-                        subArgs = [];
-                        for (prop in arg){
-                            try { // must be wrapped in try/catch to avoid 'permission denied' on some protected properties
-                                if (arg.hasOwnProperty(prop)){
-                                    subArgs.push(prop + ": " + arg[prop]);
-                                }
-                            }
-                            catch(e){}
-                        }
-                        log("----subArgs: " + subArgs);
-                     }
-                 }
-             } :
-             function(){};
-    }
-    else {
-        debug = console.debug;
-        log = console.log;
-        
-        if (debug){
-            // WebKit complains if console's debug function is called on its own
-            if (/webkit/i.test(ua)){
-                return function(){
-                    var i = 0,
-                        args = arguments,
-                        len = args.length,
-                        arr = [];
-                    
-                    if (len === 1){
-                        console.debug(args[i]);
-                    }
-                    else if (len > 1){
-                        for (; i < len; i++){
-                            arr.push(args[i]);
-                        }
-                        console.debug(arr);
-                    }
-                };
-            }
+    if (debug){
+        try {
+            debug(msg);
             return debug;
         }
-        if (log){ // old WebKit
-            if (typeof log.apply === "function"){
-                return function(){
-                    log.apply(console, arguments);
-                };
-            }
-            else { // IE8
-                return function(){
-                    var args = arguments,
-                        len = arguments.length,
-                        indent = "",
-                        i;
-                        
-                    for (i=0; i < len; i++){
-                        log(indent + args[i]);
-                        indent = "---- ";
-                    }
-                };
-            }
+        catch(e){
+            method = function(){
+                debug.apply(console, arguments);
+            };
         }
     }
+    
+    else if (log){
+        if (typeof log.apply === "function"){
+            method = function(){
+                log.apply(console, arguments);
+            };
+        }
+        else { // IE8
+            method = function(){
+                var args = arguments,
+                    len = arguments.length,
+                    indent = "",
+                    i;
+                    
+                for (i=0; i < len; i++){
+                    log(indent + args[i]);
+                    indent = "---- ";
+                }
+            };
+        }
+    }
+        
+    else if (air && air.Console && air.Console.log){
+        method = air.Console.log;
+    }
+    
+    if (method){
+        method(msg);
+        return method;
+    }
+    return function(){};
 }());
