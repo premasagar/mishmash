a679d2e9bfc3bce5c92781c15c7d813f8c011f1e
Aaron Acerboni <a.acerboni@gmail.com>
2012-03-02 13:11:10 +0000

    removed old code

--- github/github-widgets.js
+++ github/github-widgets.js
@@ -78,31 +78,3 @@
     };
 
 }(github, tim));
-
-var gh_widgets = (function(github){
-
-    var api = github;
-    var gh_widgets = {
-        html:function(user, type, callback){
-            var toHtml;
-            if (type === 'repos'){
-                toHtml = function(repos){
-                    var ul = '<ul class="github-repos">{{li}}</ul>',
-                        li = '<li>' +
-                                '<a rel="tag" href="{{html_url}}" title="{{name}} : {{description}}">{{name}}</a>' +
-                                '<br> {{description}}' +
-                             '</li>',
-                        template = '';
-
-                    for (var i = 0; i < repos.length; i++) {
-                        template += tim(li, repos[i]);
-                    };
-                    callback(tim(ul, {li:template}));
-                };
-                api.users(user).repos(toHtml);
-            }
-        }
-    };
-
-    return gh_widgets;
-}(github));
