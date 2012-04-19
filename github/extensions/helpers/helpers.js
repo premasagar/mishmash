0b42caf1c5fc253766a54bf89080eddfe36d62a7
Aaron Acerboni <a.acerboni@gmail.com>
2012-04-19 17:32:39 +0100

    Restructure helpers.js

--- github/extensions/helpers/helpers.js
+++ github/extensions/helpers/helpers.js
@@ -4,52 +4,29 @@
 
     github.helpers = {};
 
-    // http://developer.github.com/v3/events/types/
-
-    var EVENT_TYPES = [
-        'CommitCommentEvent',
-        'CreateEvent',
-        'DeleteEvent',
-        'DownloadEvent',
-        'FollowEvent',
-        'ForkEvent',
-        'ForkApplyEvent',
-        'GistEvent',
-        'GollumEvent',
-        'IssueCommentEvent',
-        'IssuesEvent',
-        'MemberEvent',
-        'PublicEvent',
-        'PullRequestEvent',
-        'PullRequestReviewCommentEvent',
-        'PushEvent',
-        'TeamAddEvent',
-        'WatchEvent'
-    ];
-
-    // Augment github.helper with the latestEvents object
-
-    github.helpers.latestEvents = {};
-
-    github.utils.each(EVENT_TYPES, function(val){
-        github.helpers.latestEvents[val] = function(user) {
-            return getEvent(user, val);
-        }
-    }, this);
-
-    function getEvent(user, event) {
+    // Requires a github user name, an event type.
+    // Option parameters:
+    // limit: a number which
+    github.helpers.events = function(user, type, options) {
         var deferred = new github.utils.deferred(),
-            promise = deferred.promise();
+            promise = deferred.promise(),
+            events = [],
+            found = 0,
+            limit = options.limit || Infinity;
 
-        github.users(user).related('events')
-            .all()
-            .pipe(function (resource) {
-                resource.data = github.utils.filter(resource.data, function(item){
-                    return (item.type === event);
-                }, this);
-                deferred.resolve(resource);
-            });
+        github.users(user + '/events').until2(function (item) {
+            if(item.type === type){
+                events.push(item);
+                found++;
+            }
+            if(found === limit){
+                return true;
+            }
+        }).then(function () {
+            deferred.resolve(events);
+        })
+
         return promise;
-    }
+    };
 
 }(github));
