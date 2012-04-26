1ba6ab24a2879fcd0230fd7d3976ffdfd3af1413
Aaron Acerboni <a.acerboni@gmail.com>
2012-04-26 17:19:19 +0100

    Add utils.prettyDate

--- github/lib/github.js
+++ github/lib/github.js
@@ -238,6 +238,31 @@ var github = (function(){
         pathname: function (url) {
             utils.a.href = url || '/';
             return utils.a.pathname;
+        },
+
+        // Takes an ISO time and returns a string representing how
+        // long ago the date represents.
+        // by John Resig
+        // http://ejohn.org/blog/javascript-pretty-date/
+        prettyDate: function prettyDate(time) {
+            var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+                diff = (((new Date()).getTime() - date.getTime()) / 1000),
+                day_diff = Math.floor(diff / 86400);
+            if ( isNaN(day_diff)){
+                return "";
+            }
+
+            return day_diff == 0 && (
+                    diff < 60 && "just now" ||
+                    diff < 120 && "1 minute ago" ||
+                    diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+                    diff < 7200 && "1 hour ago" ||
+                    diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+                day_diff == 1 && "Yesterday" ||
+                day_diff < 7 && day_diff + " days ago" ||
+                day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago" ||
+                day_diff < 365 && Math.ceil( day_diff / 30.5 ) + " months ago" ||
+                Math.ceil( day_diff / 365 ) + " years ago";
         }
     };
 
@@ -464,6 +489,7 @@ var github = (function(){
 
     var API = {
         domain: 'https://api.github.com/',
+        webRoot: 'https://github.com/',
         parameters: {
             page: 'page',
             pageValue: '1',
